c5e71a12872389624bbdb85982a6bb851609ed5a
Mateus Araujo <mateus_1542@hotmail.com>
2021-06-26 19:33:05 -0300

    chore(project): config module resolver, navigation and themes

---
+++ src/styles/themes/lightTheme.ts
@@ -0,0 +1,34 @@
+import { DefaultTheme } from 'styled-components';
+
+import { AppTheme } from './types';
+
+declare module 'styled-components' {
+  // eslint-disable-next-line no-shadow
+  export interface DefaultTheme extends AppTheme {}
+}
+
+const lightTheme: DefaultTheme = {
+  colors: {
+    primary: '#11bcaa',
+    lightPrimary: '#4dd8d0',
+    red: '#fe214d',
+    background: '#e8eaf1',
+    lightGray: '#e1e1e1',
+    gray: '#ccc',
+    mediumGray: '#666',
+    darkGray: '#333',
+    black: '#212121',
+    white: '#f9fafb',
+  },
+  font: {
+    sizes: {
+      smallest: '10px',
+      small: '12px',
+      regular: '14px',
+      big: '18px',
+      biggest: '20px',
+    },
+  },
+};
+
+export default lightTheme;
---
+++ src/styles/themes/types.ts
@@ -0,0 +1,23 @@
+export type AppTheme = {
+  colors: {
+    primary: string;
+    lightPrimary: string;
+    background: string;
+    lightGray: string;
+    gray: string;
+    darkGray: string;
+    mediumGray: string;
+    black: string;
+    white: string;
+    red: string;
+  };
+  font: {
+    sizes: {
+      smallest: string;
+      small: string;
+      regular: string;
+      big: string;
+      biggest: string;
+    };
+  };
+};
