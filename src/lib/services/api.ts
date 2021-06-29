c4025a62a20f582efe6c417ffa814cca9fb72b8f
Mateus Araujo <mateus_1542@hotmail.com>
2021-06-29 00:27:38 -0300

    chore(project): change baseUrl and app name

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'http://api.tbnb.test/api';
+export const baseURL = 'https://product-tbnb.herokuapp.com/api';
 
 const api = axios.create({ baseURL });
 
